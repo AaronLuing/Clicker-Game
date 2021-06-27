372e318fa6ca5d88ab6ac7d2444903a6db54f8f6
Aaron Luing <aaronluing@Aarons-MacBook-Air.local>
2021-06-27 06:16:49 -0500

    added annotations explaining my work for my future self

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
+// here I declare my variables
 let money = 0;
 let power = 1;
 let tool = "Teeth";
 let sold = 0;
 let victory = "You have bought all the upgrades!  There is nothing left to do but mow and collect cash.  Congratulations!"
-
+// I set a function that will increase the money variable based on the current set power.
+// the function also updates the html to display the current variable value
 function mow() {
   money += power;
   document.getElementById("money").innerHTML = "Money: $" + money
 }
-
+// this function checks the sold variable to determine if victory is achieved
 function winner() {
   if (sold == 1) {
     alert(victory)
@@ -16,7 +18,9 @@ function winner() {
     return
   }
 }
-
+// This function will upgrade the tool & power accordingly based upon current money.
+// Each Else-If adds another step in the upgrade chain, until the hardcoded SOLD OUT is achieved
+// The blank upgrade innerHTML on line 48 essentially hides the upgrade button upon ending the tree
 function upgrade() {
   if (money >= 5 && tool == "Teeth") {
     money -= 5
