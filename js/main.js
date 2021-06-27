88f17151237f7c3ee6ee69a33785b868f3bb7f25
Aaron Luing <aaronluing@Aarons-MacBook-Air.local>
2021-06-27 03:03:38 -0500

    added a victory condition, and the upgrade button will be blank once all upgrades are bought

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 let money = 0;
 let power = 1;
 let tool = "Teeth";
+let sold = 0;
+let victory = "You have bought all the upgrades!  There is nothing left to do but mow and collect cash.  Congratulations!"
 
 function mow() {
   money += power;
   document.getElementById("money").innerHTML = "Money: " + money
 }
 
-// function winner() {}
+function winner() {
+  if (sold == 1) {
+    alert(victory)
+  } else {
+    return
+  }
+}
 
 function upgrade() {
   if (money >= 5 && tool == "Teeth") {
@@ -17,6 +25,7 @@ function upgrade() {
     document.getElementById("tool").innerHTML = "Tool: " + tool
     document.getElementById("money").innerHTML = "Money: " + money
     document.getElementById("upgrade").innerHTML = "Upgrade Tool: $25"
+    winner()
   } else if (money >= 25 && tool == "Rusty Scissors") {
     money -= 25
     power += 5
@@ -24,13 +33,16 @@ function upgrade() {
     document.getElementById("tool").innerHTML = "Tool: " + tool
     document.getElementById("money").innerHTML = "Money: " + money
     document.getElementById("upgrade").innerHTML = "Upgrade Tool: $50"
+    winner()
   } else if (money >= 50 && tool == "Old Timey Mower") {
     money -= 50
     power += 15
     tool = "Battery-Powered Motor"
+    sold += 1
     document.getElementById("tool").innerHTML = "Tool: " + tool
     document.getElementById("money").innerHTML = "Money: " + money
-    document.getElementById("upgrade").innerHTML = "SOLD OUT"
+    document.getElementById("upgrade").innerHTML = ""
+    winner()
   } else if (tool == "Battery-Powered Motor") {
     alert("You bought everything!")
   } else {
